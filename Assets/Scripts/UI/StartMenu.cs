acf39863e73180efceebf1183ea053a9f4621566
Carlos <carlos.garcia.sanz05@gmail.com>
2022-02-07 20:16:06 +0100

    Menu Starting

--- Assets/Scripts/UI/StartMenu.cs
+++ Assets/Scripts/UI/StartMenu.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class StartMenu : MonoBehaviour
 {
-    [Header("References")]
+    [Header("ButtonReferences")]
     [SerializeField] Button _playButton;
+    [SerializeField] Button _exitButton;
+    [SerializeField] Button _settingsButton;
+    [SerializeField] Button _tutorialButton;
+
+    [Header("CanvasGroups")]
+    [SerializeField] CanvasGroup _mainMenu;
+    [SerializeField] CanvasGroup _tutorialMenu;
+    [SerializeField] CanvasGroup _settingsMenu;
+  
 
     LevelLoader _levelLoader;
 
@@ -13,10 +23,45 @@ public class StartMenu : MonoBehaviour
         _levelLoader = FindObjectOfType<LevelLoader>();
 
         _playButton.onClick.AddListener(LoadPlayLevel);
+        _exitButton.onClick.AddListener(Application.Quit);
+        _tutorialButton.onClick.AddListener(ShowTutorial);
+        _settingsButton.onClick.AddListener(ShowSettings);
+
     }
 
     void LoadPlayLevel()
     {
         _levelLoader.LoadLevel(_levelLoader.FirstGameLevel);
     }
+
+    void ShowTutorial()
+    {
+        _tutorialMenu.transform.DOLocalMove(Vector3.zero, 2);
+        _mainMenu.interactable = false;
+        _mainMenu.alpha = 0;
+    }
+
+    void ShowSettings()
+    {
+        _settingsMenu.transform.DOLocalMove(Vector3.zero, 2);
+        _mainMenu.interactable = false;
+        _mainMenu.alpha = 0;
+    }
+
+    void HideSettings()
+    {
+        _settingsMenu.transform.DOLocalMove(new Vector3(-2000,0,0), 2);
+        _mainMenu.interactable = true;
+        _settingsMenu.interactable = false;
+        _mainMenu.alpha = 1;
+    }
+   
+    void HideTutorial()
+    {
+        _settingsMenu.transform.DOLocalMove(new Vector3(2000,0,0), 2);
+        _mainMenu.interactable = true;
+        _tutorialMenu.interactable = false;
+        _mainMenu.alpha = 1;
+    }
+
 }
